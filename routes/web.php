01e33014534dd926d21863f29d366bbc74cc81cf
manuelleon225 <manueljhoanleonmantilla@gmail.com>
2026-04-08 20:16:31 -0500

    Creación de rutas API en api.php (estructura correcta Laravel 12)

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-use App\Http\Controllers\AuthController;
-
-Route::post('/api/register', [AuthController::class, 'register']);
-Route::post('/api/login', [AuthController::class, 'login']);
