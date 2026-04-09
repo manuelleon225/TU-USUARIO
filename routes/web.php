9166795b82ea1483fab56adf690b6247d149437d
manuelleon225 <manueljhoanleonmantilla@gmail.com>
2026-04-08 20:01:11 -0500

    Definición de rutas API en web.php para autenticación

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\AuthController;
+
+Route::post('/api/register', [AuthController::class, 'register']);
+Route::post('/api/login', [AuthController::class, 'login']);
